2f32c7d6758636eec3afeeb51ef4a212ff30d7fe
Brian Ailinger <brianjr48@yahoo.com>
2020-09-01 22:33:45 +0000

    add description

---
+++ lib/buffalo_events.rb
@@ -0,0 +1,6 @@
+require "buffalo_events/version"
+
+module BuffaloEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/buffalo_events/version.rb
@@ -0,0 +1,3 @@
+module BuffaloEvents
+  VERSION = "0.1.0"
+end
